6156d9c6dcfe961c1a3a2bfc3022df454e01a094
quillblue <sheng.qian@outlook.com>
2015-02-01 22:43:20 +0800

    GUI Designing. Daily Commit. Broken

---
+++ 12306SurveyFiller/FormMain.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SurveyFiller
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.DelayTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panelCheckSM = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnCheckSMSubmit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelSMCheckSeq = new System.Windows.Forms.Label();
+            this.labelSMCheckUserName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panelCheckSM.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DelayTimer
+            // 
+            this.DelayTimer.Interval = 1000;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(546, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(59, 26);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "浏览";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 116);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(383, 267);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // panelCheckSM
+            // 
+            this.panelCheckSM.BackColor = System.Drawing.Color.LightGray;
+            this.panelCheckSM.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelCheckSM.Controls.Add(this.label8);
+            this.panelCheckSM.Controls.Add(this.btnCheckSMSubmit);
+            this.panelCheckSM.Controls.Add(this.textBox1);
+            this.panelCheckSM.Controls.Add(this.label7);
+            this.panelCheckSM.Controls.Add(this.labelSMCheckSeq);
+            this.panelCheckSM.Controls.Add(this.labelSMCheckUserName);
+            this.panelCheckSM.Controls.Add(this.label6);
+            this.panelCheckSM.Controls.Add(this.label5);
+            this.panelCheckSM.Controls.Add(this.label1);
+            this.panelCheckSM.Location = new System.Drawing.Point(422, 227);
+            this.panelCheckSM.Name = "panelCheckSM";
+            this.panelCheckSM.Size = new System.Drawing.Size(247, 156);
+            this.panelCheckSM.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label8.Location = new System.Drawing.Point(3, 7);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 22);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "短信验证";
+            // 
+            // btnCheckSMSubmit
+            // 
+            this.btnCheckSMSubmit.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCheckSMSubmit.Location = new System.Drawing.Point(170, 96);
+            this.btnCheckSMSubmit.Name = "btnCheckSMSubmit";
+            this.btnCheckSMSubmit.Size = new System.Drawing.Size(45, 25);
+            this.btnCheckSMSubmit.TabIndex = 7;
+            this.btnCheckSMSubmit.Text = "确定";
+            this.btnCheckSMSubmit.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(96, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 26);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "123456";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(2, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 21);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "短信验证码";
+            // 
+            // labelSMCheckSeq
+            // 
+            this.labelSMCheckSeq.AutoSize = true;
+            this.labelSMCheckSeq.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSMCheckSeq.ForeColor = System.Drawing.Color.MediumBlue;
+            this.labelSMCheckSeq.Location = new System.Drawing.Point(93, 72);
+            this.labelSMCheckSeq.Name = "labelSMCheckSeq";
+            this.labelSMCheckSeq.Size = new System.Drawing.Size(63, 17);
+            this.labelSMCheckSeq.TabIndex = 4;
+            this.labelSMCheckSeq.Text = "quillqian";
+            // 
+            // labelSMCheckUserName
+            // 
+            this.labelSMCheckUserName.AutoSize = true;
+            this.labelSMCheckUserName.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSMCheckUserName.ForeColor = System.Drawing.Color.MediumBlue;
+            this.labelSMCheckUserName.Location = new System.Drawing.Point(93, 54);
+            this.labelSMCheckUserName.Name = "labelSMCheckUserName";
+            this.labelSMCheckUserName.Size = new System.Drawing.Size(63, 17);
+            this.labelSMCheckUserName.TabIndex = 3;
+            this.labelSMCheckUserName.Text = "quillqian";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(3, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "验证码序列号：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(3, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "乘车人用户名：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(3, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请在15分钟内输入相应手机上收到的验证码";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFilePath.Location = new System.Drawing.Point(133, 23);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(407, 22);
+            this.textBoxFilePath.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(15, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "任务列表";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(15, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Excel文件路径";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(15, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "满意度选择";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 446);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.panelCheckSM);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panelCheckSM.ResumeLayout(false);
+            this.panelCheckSM.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer DelayTimer;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panelCheckSM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelSMCheckUserName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelSMCheckSeq;
+        private System.Windows.Forms.Button btnCheckSMSubmit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ 12306SurveyFiller/FormMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SurveyFiller
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel97-03文件|*.xls";
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                textBoxFilePath.Text = ofd.FileName;
+            }
+        }
+
+
+    }
+}
